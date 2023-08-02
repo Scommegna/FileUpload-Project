f5cabb34fae23f47e797b5a9f904d5f64f7d8f6d
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-02 17:13:54 -0300

    feat: error handling in uploadProductImage controller

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,25 @@
 const path = require("path");
-
+const CustomError = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded.");
+  }
+
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please, upload an image.");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      `Please, upload an image with size smaller than ${maxSize}.`
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
